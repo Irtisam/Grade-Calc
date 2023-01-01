cf91653f5a6a794d5b25826afa1ae8d703104447
alvan <ydt529@usask.ca>
2023-01-01 17:50:10 -0600

    big issue fixing 2

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+#<<<<<<< HEAD
+#=======
+
+def home(request):
+    return render(request, 'calc/Home.html')
+#>>>>>>> 22df2a2 (modified templates structure)
