4bbf861c5c88b910d35570d0ff16211b3bfc2c50
alvan <ydt529@usask.ca>
2023-01-01 20:49:17 -0600

    issue fixing 3

--- calc/views.py
+++ calc/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 #<<<<<<< HEAD
 #=======
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        username = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+    context = {}
+    return render(request, 'calc/login_register.html', context)
 
 def home(request):
     return render(request, 'calc/Home.html')
