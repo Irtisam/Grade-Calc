f55859a71c84dec2837cc11822e5c2d560d4731b
alvan <ydt529@usask.ca>
2023-01-02 14:58:13 -0600

    Modifying login features

--- calc/views.py
+++ calc/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
-#<<<<<<< HEAD
-#=======
 
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
-        username = request.POST.get('password')
+        password = request.POST.get('password')
 
         try:
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or Password does not exist.")
+
     context = {}
     return render(request, 'calc/login_register.html', context)
 
+
 def home(request):
     return render(request, 'calc/Home.html')
-#>>>>>>> 22df2a2 (modified templates structure)
