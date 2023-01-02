518052bddd260bd6e88f4145c2c2f6d5b2c5cc45
alvan <ydt529@usask.ca>
2023-01-02 17:54:49 -0600

    added user signup

--- calc/urls.py
+++ calc/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('', views.home, name='home'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 
 def loginPage(request):
+    page = 'login'
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -24,12 +26,19 @@ def loginPage(request):
         else:
             messages.error(request, "Username or Password does not exist.")
 
-    context = {}
+    context = {'page':page}
     return render(request, 'calc/login_register.html', context)
+
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    return render(request, 'calc/login_register.html', {'form': form})
+
 
 def home(request):
     return render(request, 'calc/Home.html')
